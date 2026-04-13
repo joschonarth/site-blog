dd39e849a221117a20f9b1f19225eb87ab2b2d0d
joschonarth <joschonarth@gmail.com>
2026-04-13 20:17:53 -0300

    feat: implement Search component

---
+++ src/components/search/search.tsx
@@ -0,0 +1,57 @@
+import { CircleX, SearchIcon } from 'lucide-react'
+import { useRouter } from 'next/router'
+import { useCallback } from 'react'
+import { cn } from '@/lib/utils'
+
+export const Search = () => {
+  const router = useRouter()
+  const query = (router.query.q as string) ?? ''
+
+  const handleSearch = useCallback(
+    (event: React.SyntheticEvent<HTMLFormElement>) => {
+      event.preventDefault()
+      if (query.trim()) {
+        router.push(`/blog?q=${encodeURIComponent(query)}`)
+      }
+    },
+    [query, router]
+  )
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuery = event.target.value
+    router.push(`/blog?q=${encodeURIComponent(newQuery)}`, undefined, {
+      shallow: true,
+      scroll: false,
+    })
+  }
+
+  const resetSearch = () => {
+    router.push('/blog', undefined, { shallow: true, scroll: false })
+  }
+
+  return (
+    <form className="group relative w-full md:w-60" onSubmit={handleSearch}>
+      <SearchIcon
+        className={cn(
+          'absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-gray-300 transition-colors duration-200 group-focus-within:text-blue-300',
+          query ? 'text-blue-300' : ''
+        )}
+      />
+
+      <input
+        className="h-10 w-full rounded-md border border-gray-400 bg-transparent pl-9 text-body-sm text-gray-100 outline-none transition-all duration-200 placeholder:text-body-sm placeholder:text-gray-300 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 md:w-60"
+        onChange={handleQueryChange}
+        placeholder="Buscar"
+        type="text"
+        value={query}
+      />
+
+      {query && (
+        <CircleX
+          className="absolute top-1/2 right-3 h-4 w-4 -translate-y-1/2 text-gray-300"
+          onClick={resetSearch}
+        />
+      )}
+    </form>
+  )
+}
