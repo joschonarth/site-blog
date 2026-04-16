634982c557d8b706b541bb2a701be686858d55f2
joschonarth <joschonarth@gmail.com>
2026-04-16 20:19:31 -0300

    feat: create Post page

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,51 @@
+import { allPosts } from 'contentlayer/generated'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+
+export default function PostPage() {
+  const router = useRouter()
+  const slug = router.query.slug as string
+  const post = allPosts.find((post) =>
+    post.slug.toLowerCase().includes(slug.toLowerCase())
+  )
+
+  return (
+    <main className="mt-32 text-gray-100">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className="text-action-sm">
+              <Link href="/blog">Blog</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <span className="text-action-sm text-blue-200">{post?.title}</span>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <div className="grid grid-cols-1 gap-6 lg:grid-cols-[1fr_300px] lg:gap-12">
+        <article className="overflow-hidden rounded-lg border-[1px] border-gray-400 bg-gray-600">
+          <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
+            <Image
+              alt={post?.title ?? ''}
+              className="object-cover"
+              fill
+              src={post?.image ?? ''}
+            />
+          </figure>
+        </article>
+      </div>
+    </main>
+  )
+}
